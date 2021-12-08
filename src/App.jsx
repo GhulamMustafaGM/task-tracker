a72f020c48644f0c18a8bd3f219a0fa2bd8501a1
Ghulam Mustafa <mustafaji@gmail.com>
2021-12-08 15:50:35 +0100

    Added Header, Button, and Tasks Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
+const initialTasks = [
+  {
+    id: 1,
+    text: "Task 1",
+    day: "Dec 10th at 2:00PM",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Task 2",
+    day: "Dec 12th at 5:00Pm",
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: "Task 3",
+    day: "Dec 10th at 9:00AM",
+    reminder: true,
+  },
+];
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="container">
+      <Header />
+      {/* {initialTasks.map((task) => (
+        <p key={task.id}>{task.text}</p>
+      ))} */}
+
+      <Tasks tasks={initialTasks} />
+      
     </div>
   );
 }
