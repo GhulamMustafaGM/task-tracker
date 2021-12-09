c4c4549e911bba7e50cf9f5e34cb3e5527c9d200
Ghulam Mustafa <mustafaji@gmail.com>
2021-12-09 23:51:48 +0100

    updated Tasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
+import Task from "./Task";
 
-function Tasks ( { tasks } )
-{
-  if ( !tasks || tasks.length === 0 )
-  {
+function Tasks({ tasks, onToggle, onDelete }) {
+  if (!tasks || tasks.length === 0) {
     return <p>No tasks to display</p>;
   }
-  
-  return tasks.map((task) => <p key={task.id}>{task.text}</p>);
+
+  return tasks.map((task) => (
+    <Task onToggle={onToggle} onDelete={onDelete} key={task.id} task={task} />
+  ));
 }
 
 Tasks.defaultProps = {
@@ -16,6 +17,8 @@ Tasks.defaultProps = {
 
 Tasks.propTypes = {
   tasks: PropTypes.array,
+  onToggle: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Tasks;
